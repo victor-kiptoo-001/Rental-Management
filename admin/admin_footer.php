d347be98c4065682c1d3a996c2d493a633573924
victor-kiptoo-001 <veelangat680@gmail.com>
2024-12-21 07:28:06 +0300

    first commit

---
+++ admin/admin_footer.php
@@ -0,0 +1,98 @@
+<!-- /.container-fluid -->
+            <footer class="footer text-center"> <?php echo 2000+date('y');?> &copy; Nyumbani Homes </footer>
+        </div>
+        <!-- /#page-wrapper -->
+    </div>
+    <!-- /#wrapper -->
+    <!-- Jquery Cookie Handler -->
+    <script src="https://cdn.jsdelivr.net/npm/js-cookie@3.0.5/dist/js.cookie.min.js"></script>
+    <!-- jQuery -->
+    <script src="../plugins/bower_components/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="bootstrap/dist/js/tether.min.js"></script>
+    <script src="bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="../plugins/bower_components/bootstrap-extension/js/bootstrap-extension.min.js"></script>
+    <!-- Menu Plugin JavaScript -->
+    <script src="../plugins/bower_components/sidebar-nav/dist/sidebar-nav.min.js"></script>
+    <!--slimscroll JavaScript -->
+    <script src="js/jquery.slimscroll.js"></script>
+    <!--Wave Effects -->
+    <script src="js/waves.js"></script>
+    <!--Counter js -->
+    <script src="../plugins/bower_components/waypoints/lib/jquery.waypoints.js"></script>
+    <script src="../plugins/bower_components/counterup/jquery.counterup.min.js"></script>
+    <!--Morris JavaScript -->
+    <script src="../plugins/bower_components/raphael/raphael-min.js"></script>
+    <script src="../plugins/bower_components/morrisjs/morris.js"></script>
+    <!-- Custom Theme JavaScript -->
+    <script src="js/custom.min.js"></script>
+    <script src="js/dashboard1.js"></script>
+    <!-- for tables-->
+     <script src="../plugins/bower_components/datatables/jquery.dataTables.min.js"></script>
+    <!-- start - This is for export functionality only -->
+    <script src="https://cdn.datatables.net/buttons/1.2.2/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.2.2/js/buttons.flash.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js"></script>
+    <script src="https://cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/pdfmake.min.js"></script>
+    <script src="https://cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.2.2/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.2.2/js/buttons.print.min.js"></script>
+    <!-- end - This is for export functionality only -->
+    <!-- Sparkline chart JavaScript -->
+    <script src="../plugins/bower_components/jquery-sparkline/jquery.sparkline.min.js"></script>
+    <script src="../plugins/bower_components/jquery-sparkline/jquery.charts-sparkline.js"></script>
+    <script src="../plugins/bower_components/toast-master/js/jquery.toast.js"></script>
+
+    <script type="text/javascript">
+          // Theme color settings
+$(document).ready(function(){
+function store(name, val) {
+    if (typeof (Storage) !== "undefined") {
+      localStorage.setItem(name, val);
+    } else {
+      window.alert('Please use a modern browser to properly view this template!');
+    }
+  }
+ $("*[theme]").click(function(e){
+      e.preventDefault();
+        var currentStyle = $(this).attr('theme');
+
+        Cookies.set('themeselection', currentStyle, { expires: 30 });
+        store('theme', currentStyle);
+        var themeselected= Cookies.get('themeselection');
+        $('#theme').attr({href: 'css/colors/'+themeselected+'.css'})
+    });
+
+    var currentTheme = get('theme');
+    if(currentTheme)
+    {
+      $('#theme').attr({href: 'css/colors/'+currentTheme+'.css'});
+    }
+    // color selector
+    $('#themecolors').on('click', 'a', function(){
+        $('#themecolors li a').removeClass('working');
+        $(this).addClass('working')
+      });
+
+});
+
+$(document).ready(function(){
+    $("*[theme]").click(function(e){
+      e.preventDefault();
+        var currentStyle = Cookies.get('themeselection');
+        store('theme', currentStyle);
+        $('#theme').attr({href: 'css/colors/'+currentStyle+'.css'})
+    });
+
+    var currentTheme = Cookies.get('themeselection');
+    if(currentTheme)
+    {
+      $('#theme').attr({href: 'css/colors/'+currentTheme+'.css'});
+    }
+    // color selector
+$('#themecolors').on('click', 'a', function(){
+        $('#themecolors li a').removeClass('working');
+        $(this).addClass('working')
+      });
+});
+    </script>
